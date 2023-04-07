b2c824b71bcc406819a988e547015ee3a8b55a57
meparthdegama <pthreeh@outlook.com>
2023-04-07 23:08:08 +0530

    fix bug

--- app/3dview/modelconf.js
+++ app/3dview/modelconf.js
@@ -1,12 +1,12 @@
 const modelsConf = [
     {
-        path: "app/3dview/3dmodel/model0.glb",
+        path: "public/3dmodel/model0.glb",
         camara: { x: 0, y: 0, z: 110 },
         position: { x: 0, y: -10, z: 0 },
         rotation: { x: 10, y: -18, z: 0 }
     },
     {
-        path: "app/3dview/3dmodel/model3.glb",
+        path: "public/3dmodel/model3.glb",
         camara: { x: 0, y: 0, z: 50 },
         position: { x: 0, y: -5, z: 0 },
         rotation: { x: 10, y: 0, z: 0 }
